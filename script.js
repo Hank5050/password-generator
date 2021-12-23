f0620d743b25babaa5438ff67116ae62161f61d4
Christopher Hankins <Chankins5050@gmail.com>
2021-12-23 16:56:55 -0500

    added variable to javascript file

--- script.js
+++ script.js
@@ -10,5 +10,17 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var passwordLength = getLength();
+  var hasUpperCase = confirm("Would you like to include uppercase characters?");
+  var hasLowerCase = confirm("Would you like to include lowercase characters?");
+  var hasSpecial = confirm("Would you like to include special characters?");
+  var hasNumbers = confirm("Would you like to include numbers?");
+
+  var upperCasedCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  var lowerCasedCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var numericCharacters = ["0","1","2","3","4","5","6","7","8","9"]
+  var specialCharacters = ["@","%","+","\\","/","*","!","#","$","^","?",":",",","(",")","{","}","[","]","~","-","_",".","=",]
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
